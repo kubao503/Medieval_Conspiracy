c22a0e4f9f8167ea5e805d2f9bef748a81829839
o503@interia.eu <o503@interia.eu>
2023-03-09 16:45:50 +0100

    IInput interface and UserInput implementation

---
+++ Assets/Scripts/Input/UserInput.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+
+public interface IInput
+{
+    enum Axis
+    {
+        Vertical,
+        Horizontal
+    }
+
+    bool GetKeyDown(KeyCode key);
+    bool GetKeyUp(KeyCode key);
+    bool GetLeftMouseButtonDown();
+    Vector2 GetKeyAxis();
+    Vector2 GetMouseAxis();
+}
+
+
+public class UserInput : IInput
+{
+    public bool GetKeyDown(KeyCode key)
+    {
+        return Input.GetKeyDown(key);
+    }
+
+    public bool GetKeyUp(KeyCode key)
+    {
+        return Input.GetKeyUp(key);
+    }
+
+    public bool GetLeftMouseButtonDown()
+    {
+        return Input.GetMouseButtonDown(0);
+    }
+
+    public Vector2 GetKeyAxis()
+    {
+        return new Vector2()
+        {
+            x = Input.GetAxisRaw("Horizontal"),
+            y = Input.GetAxisRaw("Vertical")
+        };
+    }
+
+    public Vector2 GetMouseAxis()
+    {
+        return new Vector2()
+        {
+            x = Input.GetAxis("Mouse X"),
+            y = Input.GetAxis("Mouse Y")
+        };
+    }
+}
